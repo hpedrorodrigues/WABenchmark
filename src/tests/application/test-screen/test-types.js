bb7e04a42e366be2357d36eed7808d0998a5ea7d
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2017-10-13 16:30:14 -0300

    Fixing subtract vectors memory issues

--- src/tests/application/test-screen/test-types.js
+++ src/tests/application/test-screen/test-types.js
@@ -29,7 +29,7 @@ const TestTypes = {
             returnType: null,
             argumentTypes: ['number', 'number', 'number', 'number']
         },
-        values: [2, 4, 8, 16]
+        values: [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
     },
     M2V: {
         type: 'm2v',
--- src/tests/subtract-two-vectors/SubtractTwoVectors.c
+++ src/tests/subtract-two-vectors/SubtractTwoVectors.c
@@ -1,4 +1,4 @@
-void subtractTwoVectors(int *a, int *b, int *c, int n) {
+void subtractTwoVectors(double *a, double *b, double *c, int n) {
   for (int i = 0; i < n; i++) {
     c[i] = a[i] - b[i];
   }
